5c5cfd3d1bd798019a52bc91c6e56703fde53974
vichule <javier.cabanasdiaz@gmail.com>
2024-03-18 20:37:51 +0100

    almost done 2 first methods. some fixes left.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Room {
 
-    constructor(name, bookings, rate, discount){
+    constructor({name, bookings, rate, discount}){
         this.name = name
         this.bookings = bookings
         this.rate = rate
@@ -8,11 +8,42 @@ class Room {
     }
 
     isOccupied(date){
+        // if(typeof(date) !== 'string') throw new Error('Invalid Values');
+        if(isNaN(Date.parse(date))) throw new Error('Invalid Values');
+        const dateTransformed = new Date(date).getTime();
+
+        return this.bookings.some(booking => {
+            const dateIn = new Date(booking.checkIn).getTime();
+            const dateOut = new Date(booking.checkOut).getTime();
+            return dateIn <= dateTransformed && dateOut > dateTransformed;
+        });
        
     }
     occupancyPercentage(startDate, endDate){
+        if((isNaN(Date.parse(startDate))) || (isNaN(Date.parse(endDate)))){
+            throw new Error('Invalid Values');
+        }
+        if(Date.parse(startDate) > Date.parse(endDate)){
+            throw new Error('Invalid Values. Start Date cant be greater than End Date');
+        }
+
+        const firstDate = new Date(startDate)
+        const LastDate = new Date(endDate)
+        let days = 0
+        let daysOccupied = 0
+
+        while(firstDate <= LastDate){
+            days += 1
+            if(this.isOccupied(firstDate.toString())){
+                daysOccupied += 1
+            }
+            firstDate.setDate(firstDate.getDate()+1)
+            
+        }
+        return Math.round((daysOccupied / days)* 100)
 
     }
+
 
     static totalOccupancyPercentage(rooms, startDate, endDate) {
 
@@ -24,7 +55,7 @@ class Room {
 }
 
 class Booking {
-    constructor(name, email, checkIn, checkOut, discount, room){
+    constructor({name, email, checkIn, checkOut, discount, room}){
         this.name = name
         this.email = email
         this.checkIn = checkIn
--- index.test.js
+++ index.test.js
@@ -1,48 +1,57 @@
-const {Room, Booking} = require('./index.js')
+    const {Room, Booking} = require('./index.js')
 
-const roomTemplate = {};
-const bookingTemplate = {name: 'Javi C.D', email: 'fakeemail@gmail.com'};
+    const roomTemplate = {name: 'roomA', bookings: [], rate:10, discount: 0};
+    const bookingTemplate = {name: 'Javi C.D', email: 'fakeemail@gmail.com'};
 
-const room = new Room({...roomTemplate})
-const bookingA = new Booking({...bookingTemplate, checkIn: '10/10/2024', checkOut:'15/10/2024', room})
-const bookingB = new Booking({...bookingTemplate, checkIn: '2024/10/15', checkOut:'2024/10/20', room})
-const bookingC = new Booking({...bookingTemplate, checkIn: '2024/10/20', checkOut:'2024/10/30', room})
-room.bookings = [bookingA, bookingB, bookingC]
-
-
-test('Rooms is occupied inside range', () => {
-    expect(room1.isOccupied('12/10/2024')).toBeTruthy();
-});
-
-test('Rooms is occupied inside range', () => {
-    expect(room.isOccupied('2024/10/20')).toBeTruthy();
-});
-
-test('Rooms is occupied outside range', () => {
-    expect(room.isOccupied('2024/12/02')).toBeFalsy();
-});
-
-test('Rooms is occupied outside range', () => {
-    expect(room.isOccupied('2024/09/02')).toBeFalsy();
-});
-
-test('Return correct percentage in range of dates', () => {
-    expect(room.occupancyPercentage()).toBe(100);
-});
+    const room = new Room({...roomTemplate})
+    const bookingA = new Booking({...bookingTemplate, checkIn: '2024-10-10', checkOut:'2024-10-15', room})
+    const bookingB = new Booking({...bookingTemplate, checkIn: '2024-10-15', checkOut:'2024-10-20', room})
+    const bookingC = new Booking({...bookingTemplate, checkIn: '2024-10-20', checkOut:'2024-10-30', room})
+    room.bookings = [bookingA, bookingB, bookingC]
 
 
-test('Return correct total occupancy percentage for all rooms', () => {
-    expect(Room.totalOccupancyPercentage()).toBeTruthy();
-});
+    test('Rooms is occupied inside range', () => {
+        expect(room.isOccupied('2024-10-12')).toBeTruthy();
+    });
 
+    test('Rooms is occupied inside range', () => {
+        expect(room.isOccupied('2024-10-20')).toBeTruthy();
+    });
 
-test('Return available rooms between date1 & date2', () => {
-    expect(Room.availableRooms()).toBe(3);
-});
+    test('Rooms is occupied outside range', () => {
+        expect(room.isOccupied('2024-12-02')).toBeFalsy();
+    });
+
+    test('Rooms is occupied outside range', () => {
+        expect(room.isOccupied('2024-09-02')).toBeFalsy();
+    });
+
+    test('Return correct percentage of days within range of dates given', () => {
+        expect(room.occupancyPercentage('2024-10-10','2024-10-15')).toBe(100);
+    });
+
+    test('Return correct percentage of days within range of dates given', () => {
+        expect(room.occupancyPercentage('2024-11-30','2024-12-20')).toBe(0);
+    });
+
+    test('Return correct percentage of days within range of dates given', () => {
+        expect(room.occupancyPercentage('2024-10-08','2024-10-11')).toBe(50);
+    });
 
 
 
-test('Return a total fee of for the booking: ', () => {
-    expect(booking.getFee().toBe(100));
-});
+    test('Return correct total occupancy percentage for all rooms', () => {
+        expect(Room.totalOccupancyPercentage()).toBeTruthy();
+    });
+
+
+    test('Return available rooms between date1 & date2', () => {
+        expect(Room.availableRooms()).toBe(3);
+    });
+
+
+
+    test('Return a total fee of for the booking: ', () => {
+        expect(booking.getFee().toBe(100));
+    });
 
